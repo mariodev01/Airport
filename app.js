494aae67f5a55f18b98279e02c4c357c0bca005e
mariodev01 <mariovalentinomata@gmail.com>
2026-05-10 19:10:41 -0400

    nose

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Convecete, toy aqui bebe");
+const btn = document.getElementById("btnShow");
+
+btn.addEventListener('click',()=>{
+    console.log("Como podria irme lejos de tu amor?");
+});
+
+
+//console.log(btn);
+
+//console.log("Convecete, toy aqui bebe");
